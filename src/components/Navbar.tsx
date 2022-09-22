fa298f54a7ec43fca9498afe3d02357865bacfa8
Lawrence Good <lawrence.good55@gmail.com>
2022-09-21 18:58:02 -0700

    Set structure, added eslint for astro files

--- src/components/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-	return (
-		<div>
-			<img src="/logo.webp" />
-		</div>
-	)
-}
---
+++ src/routes.ts
@@ -0,0 +1,22 @@
+export default [
+	{
+		name: 'Home',
+		path: '/',
+	},
+	{
+		name: 'Our Services',
+		path: '/our-services',
+	},
+	{
+		name: 'Testimonials',
+		path: '/testimonials',
+	},
+	{
+		name: 'About Us',
+		path: '/about-us',
+	},
+	{
+		name: 'Contact Us',
+		path: '/contact-us',
+	},
+]
