13ae463f246107161af3ad35a3414b5886a03e00
Lawrence Good <lawrence.good55@gmail.com>
2022-09-23 09:14:28 -0700

    Layout and home page complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-
 import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind()],
+	// Use to generate the sitemap and canonical URLs in the final build
+	site: 'https://purafog.com',
+	integrations: [react(), tailwind(), image(), sitemap()],
 })
